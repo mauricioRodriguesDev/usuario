ef0678db733dadbdae0a73b676dfca11a475aeff
Mauricio Rodrigues <mauriciorodrigues.dev94@gmail.com>
2025-11-18 16:45:39 -0300

    adicionados metodo Get, Delete e Login

---
+++ src/main/java/com/javanauta/usuario/infrastructure/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,16 @@
+package com.javanauta.usuario.infrastructure.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String mensagem){
+        super(mensagem);
+
+    }
+
+    public ResourceNotFoundException (String mensagem, Throwable throwable){
+        super(mensagem, throwable);
+
+    }
+}
+
+
